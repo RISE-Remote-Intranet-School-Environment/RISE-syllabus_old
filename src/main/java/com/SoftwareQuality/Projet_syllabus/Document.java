6fa7e751d2c0b50a1018fe11a1c9fd5f527666b0
raphaeljadin <21295@ecam.be>
2021-11-19 09:16:58 +0100

    Add instance variable class Document

--- src/main/java/com/SoftwareQuality/Projet_syllabus/Document.java
+++ src/main/java/com/SoftwareQuality/Projet_syllabus/Document.java
@@ -1,4 +1,13 @@
 package com.SoftwareQuality.Projet_syllabus;
 
+import java.util.Date;
+
 public class Document {
+    private String name;
+    private String author;
+    private Date publish_date;
+    private int Pages;
+
+    //getters et setters de la classe
+
 }
