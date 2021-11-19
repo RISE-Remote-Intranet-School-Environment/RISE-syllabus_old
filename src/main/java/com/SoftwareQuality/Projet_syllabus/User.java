9ab448db9f4ba87bcecd0812ebab8d2ca401d1f2
GaetanHHHHHH <gaetanherinne@gmail.com>
2021-11-19 09:39:45 +0100

    Add builder and get/set methods

--- src/main/java/com/SoftwareQuality/Projet_syllabus/User.java
+++ src/main/java/com/SoftwareQuality/Projet_syllabus/User.java
@@ -1,5 +1,39 @@
 package com.SoftwareQuality.Projet_syllabus;
 
 public class User {
+    private String FirstName;
+    private String LastName;
+    private String Email;
 
+    //constructeur
+    public User(String FirstName, String LastName, String Email) {
+        this.FirstName = FirstName;
+        this.LastName = LastName;
+        this.Email = Email;
+    }
+
+    //getters et setters
+    public void setFirstName(String FirstName) {
+        this.FirstName = FirstName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public String getLastName() {
+        return FirstName;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
 }
