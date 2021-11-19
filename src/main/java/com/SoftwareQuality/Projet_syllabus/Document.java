c5bdf0bc425005fa054bb141f7c725561a555983
raphaeljadin <21295@ecam.be>
2021-11-19 09:25:46 +0100

    Add instance variables, cobstructor and getters/setters to class Document

--- src/main/java/com/SoftwareQuality/Projet_syllabus/Document.java
+++ src/main/java/com/SoftwareQuality/Projet_syllabus/Document.java
@@ -6,8 +6,40 @@ public class Document {
     private String name;
     private String author;
     private Date publish_date;
-    private int Pages;
+    private int pages;
+
+    //constructeur
+    public Document(String name, String author, Date publish_date, int pages){
+        this.name = name;
+        this.author = author;
+        this.publish_date = publish_date;
+        this.pages = pages;
+    }
 
     //getters et setters de la classe
-
+    public String getName() {
+        return name;
+    }
+    public String getAuthor() {
+        return author;
+    }
+    public Date getPublish_date() {
+        return publish_date;
+    }
+    public int getPages() {
+        return pages;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    public void setPublish_date(Date publish_date) {
+        this.publish_date = publish_date;
+    }
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+    //fin des getters et setters
 }
