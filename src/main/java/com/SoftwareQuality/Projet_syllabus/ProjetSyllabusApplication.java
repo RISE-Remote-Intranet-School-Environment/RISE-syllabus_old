66f188a8fb4e5b7925c8d53a2e2413ffa4965de2
Seb1903 <sebastien.martinez99@gmail.com>
2021-11-26 10:22:28 +0100

    Create database class

--- src/main/java/com/SoftwareQuality/Projet_syllabus/ProjetSyllabusApplication.java
+++ src/main/java/com/SoftwareQuality/Projet_syllabus/ProjetSyllabusApplication.java
@@ -3,11 +3,19 @@ package com.SoftwareQuality.Projet_syllabus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.SQLException;
+
 @SpringBootApplication
 public class ProjetSyllabusApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProjetSyllabusApplication.class, args);
+	public ProjetSyllabusApplication()  {
 	}
+
+	public static void main(String[] args) throws SQLException {
+		SpringApplication.run(ProjetSyllabusApplication.class, args);
+		Database db = new Database();
+		db.getData("document");
+	}
+
 
 }
