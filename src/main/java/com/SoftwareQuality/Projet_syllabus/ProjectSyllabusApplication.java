627b08882ce1429a25d3283af36f854124e67772
16205 <16205@ecam.be>
2021-11-26 09:08:31 +0100

    Refactor main class

--- src/main/java/com/SoftwareQuality/Projet_syllabus/ProjectSyllabusApplication.java
+++ src/main/java/com/SoftwareQuality/Projet_syllabus/ProjectSyllabusApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProjetSyllabusApplication {
+public class ProjectSyllabusApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProjetSyllabusApplication.class, args);
+		SpringApplication.run(ProjectSyllabusApplication.class, args);
 	}
 
 }
