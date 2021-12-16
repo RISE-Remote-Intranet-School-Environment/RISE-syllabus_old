346f86e7bad7b7c8139500e1924b2f45109c28bf
16205 <16205@ecam.be>
2021-12-16 13:40:36 +0100

    Refactor class attributes to match with user database

--- src/main/java/com/SoftwareQuality/Projet_syllabus/User.java
+++ src/main/java/com/SoftwareQuality/Projet_syllabus/User.java
@@ -1,25 +1,33 @@
 package com.SoftwareQuality.Projet_syllabus;
 
 public class User {
+    private int ID;
     private String firstName;
     private String lastName;
     private String email;
+    private String role;
 
     /**
      * Constructor of user object
-     * @param firstName user's firstname
-     * @param lastName user's lastname
-     * @param email user's email address
+     * @param ID User ID
+     * @param firstName User first name
+     * @param lastName User last name
+     * @param email User email address
+     * @param role User role
      */
-    public User(String firstName, String lastName, String email) {
+    public User(int ID, String firstName, String lastName, String email, String role) {
+        this.ID = ID;
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
+        this.role = role;
     }
 
     //getters et setters
-    public void setFirstName(String FirstName) {
-        this.firstName = FirstName;
+    public void setID(int ID) { this.ID = ID; }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
 
     public void setLastName(String lastName) {
@@ -29,6 +37,10 @@ public class User {
     public void setEmail(String email) {
         this.email = email;
     }
+
+    public void setRole(String role) { this.role = role; }
+
+    public int getId() { return ID; }
 
     public String getFirstName() {
         return firstName;
@@ -41,4 +53,6 @@ public class User {
     public String getEmail() {
         return email;
     }
+
+    public String getRole() { return role; }
 }
